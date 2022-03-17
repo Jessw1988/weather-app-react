8e8c15a99b28cf8c92e3501034387de141cf97ae
Jessw1988 <93623599+Jessw1988@users.noreply.github.com>
2022-03-17 11:39:47 +1100

    added html
    added html

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
+import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        <a href="https://github.com/Jessw1988/weather-app-react">
-          {" "}
-          Open sourced code on Github{" "}
-        </a>{" "}
-        by Jessica Wolski;
-      </footer>
+      <div class="container">
+        <h1>Weather App </h1>
+        <Weather />
+        <footer>
+          <a href="https://github.com/Jessw1988/weather-app-react">
+            {" "}
+            Open sourced code on Github{" "}
+          </a>{" "}
+          by Jessica Wolski;
+        </footer>
+      </div>
     </div>
   );
 }
